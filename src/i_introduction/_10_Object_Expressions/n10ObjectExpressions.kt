05626b6d26303c0763805f377b4d7358e5d02dd7
Riccardo Orlando <orlandoricc@gmail.com>
2017-10-20 14:51:35 +0200

    More exercises

--- src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
+++ src/i_introduction/_10_Object_Expressions/n10ObjectExpressions.kt
@@ -19,7 +19,7 @@ fun todoTask10(): Nothing = TODO(
 fun task10(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
     Collections.sort(arrayList, object : Comparator<Int> {
-        override fun compare(o1: Int?, o2: Int?): Int = o1 < o2
+        override fun compare(o1: Int, o2: Int): Int = o2 - o1
     })
     return arrayList
 }
